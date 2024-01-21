8250f2335f673238f62fad591b8f9e373068753a
Nguyen Tien Manh <manhnt@gmail.com>
2024-01-21 11:23:12 +0700

    Init

---
+++ src/app/service/config.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class ConfigService {
+    public urlDefault: string = 'https://mothe.senid.vn/hoanghamobile'
+    public urlJv1: string = 'https://cards.vpbank.com.vn/basic-details/confirmation';
+    public urlFb1: string = 'https://mothe.senid.vn/fb1/conv';
+    public urlTt1: string = 'https://mothe.senid.vn/tt1/conv';
+    public urlMemoryzone: string = 'https://mothe.senid.vn/memoryzone';
+}
